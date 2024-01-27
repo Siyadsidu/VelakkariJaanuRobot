1a8e767b6e69fcb1f64d12235489e00f1c0b7daf
Siyadsidu <157882794+Siyadsidu@users.noreply.github.com>
2024-01-27 18:31:45 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/VJBots/AutoFilterAdvance /VelakkariJaanuRobot
+  git clone https://github.com/Siyadsidu/VelakkariJaanuRobot /VelakkariJaanuRobot
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
   git clone $UPSTREAM_REPO /VelakkariJaanuRobot
